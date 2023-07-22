9cb0bc63f11faa20ba0bb746c0e9577c50591a2f
Bibhav Lamichhane <bibhav.25@gmail.com>
2023-07-22 20:50:34 +0545

    Front page modified

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 //8638f93bb029f8f12a54b1b33119a1ab8c02d9aa7cf04c7b9f14fb433e72e943
 //0068b7fc089186eae2f64ee425ee828d6e7d96bcb7a995b1853d5084c2f5a35b
+//fd00664621d4317710304aaf9f0a38c17f7d5870e4b0a784c2ace4d9e8adf7dd
 
 
 app.use((req, res, next) => {
@@ -27,19 +28,25 @@ app.get("/api", (req, res) => {
     const ltype = req.query.ltype;
     const location = req.query.location;
 
-    if(pagination) axios.get(`https://serpapi.com/search?engine=google_jobs&api_key=0068b7fc089186eae2f64ee425ee828d6e7d96bcb7a995b1853d5084c2f5a35b&q=${query}&start=${pagination}&ltype=${ltype}&location=${location}`)
-    .then(response => {res.json(response.data)});
+    try {
+        // if(pagination) axios.get(`https://serpapi.com/search?engine=google_jobs&api_key=fd00664621d4317710304aaf9f0a38c17f7d5870e4b0a784c2ace4d9e8adf7dd&q=${query}&start=${pagination}&ltype=${ltype}&location=${location}`)
+        // .then(response => {res.json(response.data)});
+        
+        axios.get(`https://serpapi.com/search?engine=google_jobs&api_key=fd00664621d4317710304aaf9f0a38c17f7d5870e4b0a784c2ace4d9e8adf7dd&q=${query}&location=${location}`)
+        .then(response => {res.json(response.data); console.log(response.data)});
 
-    else axios.get(`https://serpapi.com/search?engine=google_jobs&api_key=0068b7fc089186eae2f64ee425ee828d6e7d96bcb7a995b1853d5084c2f5a35b&q=${query}&location=${location}`)
-    .then(response => {res.json(response.data)});
-
+    }
+    catch(err) {throw new Error(err)};
+    
+    
+   
     
 })
 
 app.get("/apply", (req, res) => {
     const query = req.query.query
 
-    axios.get(`https://serpapi.com/search?engine=google_jobs_listing&api_key=0068b7fc089186eae2f64ee425ee828d6e7d96bcb7a995b1853d5084c2f5a35b&q=${query}`)
+    axios.get(`https://serpapi.com/search?engine=google_jobs_listing&api_key=fd00664621d4317710304aaf9f0a38c17f7d5870e4b0a784c2ace4d9e8adf7dd&q=${query}`)
     .then(response => {res.json(response.data)});
 
     
